0a98f27aff527feccb15d7a47257f3e236bd8b31
Pablo Gomez <pgomez@invertironline.com>
2024-05-24 00:24:05 -0300

    vv

--- infrastructure/terraform/script.sh
+++ infrastructure/terraform/script.sh
@@ -17,6 +17,9 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker --version
 docker-compose --version
 
+# Configurar Nginx con un certificado SSL autofirmado
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=localhost"
+
 # Configurar Nginx
 sudo cat <<EOL > /etc/nginx/sites-available/default
 server {
