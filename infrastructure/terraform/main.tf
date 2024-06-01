7f0fdabf42c1ff7fe9181c8012e905443e6f1756
Pablo Gomez <pgomez@invertironline.com>
2024-06-01 11:36:14 -0300

    lb

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -135,7 +135,7 @@ resource "aws_lb_target_group" "tg_todo_list" {
 # Crea un Listener para HTTP (puerto 80)
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.lb_todo_list.arn
-  port              = 3000
+  port              = 80
   protocol          = "HTTP"
 
   # default_action {
@@ -152,6 +152,12 @@ resource "aws_lb_listener" "http" {
     
     target_group_arn = aws_lb_target_group.tg_todo_list.arn
   }
+}
+
+resource "aws_lb_target_group_attachment" "tg_attachment" {
+  target_group_arn = aws_lb_target_group.tg_todo_list.arn
+  target_id        = aws_instance.ec2_todo_list.id
+  port             = 3000
 }
 
 # Crea una regla de enrutamiento para redirigir el tráfico HTTPS al Target Group
