6fb05a0ce79b7a264ce8782c4b5bbfb37e7664d1
Pablo Gomez <pgomez@invertironline.com>
2024-06-11 00:11:21 -0300

    se agrega hostname

--- app/src/server.js
+++ app/src/server.js
@@ -3,14 +3,16 @@ const itemsRouter = require('./routes/items');
 const db = require('./persistence');
 const logger = require('./logger');
 
+const host = process.env.HOSTNAME;
+
 // Rutas
 app.use('/items', itemsRouter);
 
 db.init().then(() => {
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
-        logger.info(`Server is running on port ${port}`);
-        console.log(`Listening on port ${port}`);
+        logger.info(`Server is running on port ${port}. Host: ${host}`);
+        console.log(`Listening on port ${port}. Host: ${host}`);
     });
 }).catch((err) => {
     logger.error('An error has ocurred', { err });
